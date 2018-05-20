0fae04308e720e55a9b99d7b1ce2ffcca2ef1193
Aaron Christopher-Scott <s5063707@bournemouth.ac.uk>
2018-05-20 23:15:32 +0100

    Initial commit

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+
+    private void OnMouseUp()
+    {
+        if (gameObject.tag == "Restart")
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        if (gameObject.tag == "Finish")
+        {
+            Application.Quit();
+        }
+    }
+}
